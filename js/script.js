aafc17bdc32047d81f8806c8a9e5a4423bc4cf74
Marcos <xbrcvar@gmail.com>
2024-06-17 23:38:22 -0300

    Adicionar estrutura inicial do jogo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.gameConfig = {
+  enemies: 7,
+  carHeight: 150, // Altura do elemento em pixel
+  carColors: [50, 80, 150, 180, 260, 320].sort(() => .5 - Math.round()),
+
+  started: false,
+  score: 0,
+  speed: null,
+  traffic: null,
+};
