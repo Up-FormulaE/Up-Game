6d2095f5a92c2e390ca37f8d0d13b24efc3f2f30
Marcos <xbrcvar@gmail.com>
2024-06-17 23:39:24 -0300

    Adicionar eventos de teclas

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ window.gameConfig = {
   speed: null,
   traffic: null,
 };
+
+const scoreElement = document.querySelector('.score')
+const gameRunwayElement = document.getElementById('game')
+const playerCarElement = document.createElement('div')
+playerCarElement.classList.add("car");
+
+const menuButtons = document.querySelectorAll('.menu > button')
+menuButtons.forEach((btn) => btn.addEventListener('click', start));
+
+
+const defaultKeys = { w: false, a: false, s: false, d: false };
+document.addEventListener("keydown", startMovement);
+document.addEventListener("keyup", stopMovement);
+
+function startMovement(event) {
+  if (event.key in defaultKeys) defaultKeys[event.key] = true;
+}
+
+function stopMovement(event) {
+  if (event.key in defaultKeys) defaultKeys[event.key] = false;
+}
+
